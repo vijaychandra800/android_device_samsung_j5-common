be6bc3dc89560e521678ca3691d20be7af80d89a
Nick Verse <nikoskalomiris@gmal.com>
2016-03-21 13:08:25 +0200

    updated prima wlan module

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,6 +111,15 @@ TARGET_USES_WCNSS_CTRL 		 := true
 WIFI_DRIVER_FW_PATH_AP 		 := "ap"
 WIFI_DRIVER_FW_PATH_STA 	 := "sta"
 WPA_SUPPLICANT_VERSION 		 := VER_0_8_X
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+
+WLAN_MODULES:
+	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
