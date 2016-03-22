c68c3aafc29b1cd868a9374100e3c43bfa279eae
Nick Verse <nikoskalomiris@gmal.com>
2016-03-22 13:31:18 +0200

    update

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-SAMSUNG_TARGETS := a5ultexx a5ltexx
+SAMSUNG_TARGETS := j5nltexx j5ltexx
 
 ifneq ($(filter $(SAMSUNG_TARGETS),$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
