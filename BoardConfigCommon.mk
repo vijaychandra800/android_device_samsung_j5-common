8604ad1ee92f6d0335f76c508c188aca7c22a4cd
Nick Verse <nikoskalomiris@gmail.com>
2016-03-28 21:57:13 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,9 +199,6 @@ TW_TARGET_USES_QCOM_BSP := true
 # Keys
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-# MTP
-BOARD_MTP_DEVICE := "/dev/usb_mtp_gadget"
-
 # Storage
 #TARGET_RECOVERY_FSTAB := device/samsung/j5-common/recovery/twrp.fstab
 TARGET_RECOVERY_FSTAB := device/samsung/j5-common/rootdir/etc/fstab.qcom
@@ -210,7 +207,7 @@ TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_NO_USB_STORAGE := true
-#TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_MTP_DEVICE := "/dev/mtp_usb"
 RECOVERY_SDCARD_ON_DATA := true
 #RECOVERY_VARIANT := twrp
 
