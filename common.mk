fa7e5923e9abe34acb135769421a3869d1805a50
Nick Verse <nikoskalomiris@gmail.com>
2016-04-27 19:33:02 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -116,6 +116,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     qcmediaplayer
+    
+# Config files for hostapd
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/configs/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny
 
 # FM
 PRODUCT_PACKAGES += \
