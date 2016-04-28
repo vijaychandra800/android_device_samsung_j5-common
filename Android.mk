d7723613ca966392e09dcdc202d968a8b1a7a558
Nick Verse <nikoskalomiris@gmail.com>
2016-04-28 17:17:33 +0300

    Commonize symlinks for all variants

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-SAMSUNG_TARGETS := j5nltexx
+SAMSUNG_TARGETS := j5nltexx j5ltexx
 
 ifneq ($(filter $(SAMSUNG_TARGETS),$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
