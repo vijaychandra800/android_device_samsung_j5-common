4a25dd187c741a239da4365f4d887aa5f06a6e2b
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-04-29 11:34:13 +0530

    correction

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-SAMSUNG_TARGETS := j5nltexx j5ltexx j500h
+SAMSUNG_TARGETS := j5nltexx j5ltexx j53gxx
 
 ifneq ($(filter $(SAMSUNG_TARGETS),$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
