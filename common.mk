3ac81e99014192267319515926b90425bd667871
Nick Verse <nikoskalomiris@gmail.com>
2016-06-05 17:39:35 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -52,8 +52,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
-    audio.primary.msm8916 \
-    audio_policy.default \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
