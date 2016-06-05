38c4961eb4261c0a14aeb2b31b793468195a3770
nickverse <nikoskalomiris@gmail.com>
2016-06-05 18:30:11 +0300

    Add InputDisabler v2.0 for testing

--- InputDisabler/src/org/davis/inputdisabler/utils/Constants.java
+++ InputDisabler/src/org/davis/inputdisabler/utils/Constants.java
@@ -1,19 +1,20 @@
-package org.davis.inputdisabler;
+package org.davis.inputdisabler.utils;
 
 /*
  * Created by Dāvis Mālnieks on 04/10/2015
  */
 
 public final class Constants {
-    private Constants() {
 
-    }
+    // Paths to input devices
 
     public static final String TS_PATH = "/sys/class/input/input4/enabled";
 
     public static final String TK_PATH = "/sys/class/input/input2/enabled";
 
-	// Doze intent
+    // Doze
 
-	public static final String ACTION_DOZE_PULSE_STARTING = "android.intent.action.DOZE_PULSE_STARTING";
+    public static final int DOZING_TIME = 1000 * 5;
+
+    public static final String ACTION_DOZE_PULSE_STARTING = "android.intent.action.DOZE_PULSE_STARTING";
 }
