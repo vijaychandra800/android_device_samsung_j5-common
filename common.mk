06caa04eb8ef6e144a44f1f8d55c05ad8b25c250
Nick Verse <nikoskalomiris@gmail.com>
2016-06-07 15:42:21 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -53,13 +53,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.primary.msm8916 \
+    audio.primary.default \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing
+    libqcomvoiceprocessing \
+    libaudiopolicymanager
 
 # TinyAlsa utils
 PRODUCT_PACKAGES += \
