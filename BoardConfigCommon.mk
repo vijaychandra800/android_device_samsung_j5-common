2999576a74f0001625639b29dbc2903e6a5a029b
Nick Verse <nikoskalomiris@gmail.com>
2016-06-12 05:01:42 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,6 +164,9 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     bluetooth_loader.te \
     file_contexts \
+    thermal_engine.te \
+    irsc_util.te \
+    perfd.te \
     system_server.te \
     mediaserver.te \
     property.te \
