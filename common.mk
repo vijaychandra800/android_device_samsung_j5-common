1d96c137bd830022644286b90a9331d160910069
Nick Verse <nikoskalomiris@gmail.com>
2016-06-17 02:19:18 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -59,7 +59,6 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libaudioroute \
     libaudiopolicymanager \
     libqcomvoiceprocessing
 
