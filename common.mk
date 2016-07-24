6da3fe0745ac457439606a734740c52a11c084f2
ganeshi4u <ganeshvarmai4u@gmail.com>
2016-07-24 12:44:22 +0530

    FM config for MM

--- common.mk
+++ common.mk
@@ -120,10 +120,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
