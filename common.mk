6eb0cd93c2adef43eaef7da8ef52a76891fc6ce6
Nick Verse <nikoskalomiris@gmail.com>
2016-09-06 03:04:12 +0300

    Test for audio (1)

--- common.mk
+++ common.mk
@@ -52,11 +52,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.primary.msm8916 \
-    audio_policy.msm8916 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
+    libaudiopolicymanager \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
