f1ba70e05f13f62bdd76e22cb3e1bb39c2f9b319
Nick Verse <nikoskalomiris@gmail.com>
2016-09-27 16:09:27 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -23,7 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/j5-common/overlay
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
