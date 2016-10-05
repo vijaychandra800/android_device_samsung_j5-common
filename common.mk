cca9e5e905c1cd1eea17ad65f6d1be6f4fbceb2b
Nick Verse <nikoskalomiris@gmail.com>
2016-10-05 16:33:13 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -167,7 +167,8 @@ PRODUCT_PACKAGES += \
     init.crda.sh \
     init.qcom.bt.sh \
     init.qcom.coex.sh \
-    init.qcom.fm.sh
+    init.qcom.fm.sh \
+    init.qcom.uicc.sh
 
 # Ramdisk
 PRODUCT_PACKAGES += \
