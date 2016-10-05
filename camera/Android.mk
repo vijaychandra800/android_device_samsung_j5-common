8cebe84048085433af4d4f3f45db79d6d566e5ae
nickverse <nikoskalomiris@gmail.com>
2016-10-05 17:01:11 +0300

    Add device specific Camera HAL for testing

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,21 +1,23 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+ifeq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+MM_V4L2_DRIVER_LIST += msm8960
+MM_V4L2_DRIVER_LIST += msm8974
+MM_V4L2_DRIVER_LIST += msm8916
+MM_V4L2_DRIVER_LIST += msm8226
+MM_V4L2_DRIVER_LIST += msm8610
+MM_V4L2_DRIVER_LIST += apq8084
+MM_V4L2_DRIVER_LIST += mpq8092
+MM_V4L2_DRIVER_LIST += msm_bronze
+MM_V4L2_DRIVER_LIST += msm8916
+MM_V4L2_DRIVER_LIST += msm8994
+MM_V4L2_DRIVER_LIST += msm8084
 
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
+ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
+  ifneq ($(USE_CAMERA_STUB),true)
+    ifneq ($(BUILD_TINY_ANDROID),true)
+      include $(call all-subdir-makefiles)
+    endif
+  endif
+endif
 
 endif
