26bd246ff5ef15550fff40ee1b54face72f29f67
Nick Verse <nikoskalomiris@gmail.com>
2016-10-07 21:12:27 +0300

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 
 # Configuration scripts
 include $(CLEAR_VARS)
+LOCAL_MODULE       := hcidump.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/hcidump.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.wifi.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
