d00256c206db9070f984cdb6e8045aee7d3166e2
Nick Verse <nikoskalomiris@gmail.com>
2016-10-17 02:11:56 +0300

    Fix for firmware

--- Android.mk
+++ Android.mk
@@ -89,7 +89,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(PLAYREADY_SYMLINKS)
 
 # Prov
 PROV_IMAGES := \
-    prov.b00 prov.b01 prov.b02 prov.b03 prov.mtd \
+    prov.b00 prov.b01 prov.b02 prov.b03 prov.mdt \
 
 PROV_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(PROV_IMAGES)))
 $(PROV_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -250,10 +250,11 @@ ALL_DEFAULT_INSTALLED_MODULES += $(WV_SYMLINKS)
 # Modem
 MODEM_IMAGES := \
     modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
-    modem.b06 modem.b07 modem.b08 modem.b10 modem.b11 modem.b12 \
-    modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 modem.b18 \
-    modem.b19 modem.b20 modem.b21 modem.b23 modem.b24 modem.b25 \
-    modem.b27 modem.b28 modem.mdt mba.mbn
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.b11 \
+    modem.b12 modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 \
+    modem.b18 modem.b19 modem.b20 modem.b21 modem.b22 modem.b23 \
+    modem.b24 modem.b25 modem.b26 modem.b27 modem.b28 modem.mdt \
+    mba.mbn
 
 MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_IMAGES)))
 $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
