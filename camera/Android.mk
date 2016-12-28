c255d87c72940ad9780ea24ea4025423e67dc26a
Nick Verse <nikoskalomiris@gmail.com>
2016-12-28 10:24:02 +0200

    camera_wrapper: include libcutils in shared libraries

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,7 +10,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils libcutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
